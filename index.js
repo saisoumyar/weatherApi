28d8f255d1feaedfc722f3c5525dcfef101226f0
Sai Soumya Rout <48159593+saisoumyar@users.noreply.github.com>
2021-09-17 12:04:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const params = {
+  access_key: '1bc1ddcc6a5d9cc3f425c05aab8134e9',
+  query: 'bhadrak'
+}
+
+axios.get('http://api.weatherstack.com/current', {params})
+  .then(response => {
+    const apiResponse = response.data;
+    console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
+  }).catch(error => {
+    console.log(error);
+  });
